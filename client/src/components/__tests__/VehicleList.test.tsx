ac1f23e99c75a40ada576547f90fcc544e662de7
Adi <adityabev@gmail.com>
2020-11-22 00:41:35 +0000

    Add tests

--- client/src/components/__tests__/VehicleList.test.tsx
+++ client/src/components/__tests__/VehicleList.test.tsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Theme from "../../Theme";
-import VehicleList from "../VehicleList";
+import VehicleList, { apiUrl } from "../VehicleList";
+import { fetchData } from "../../utils/fetchData";
 
-test("Render <VehicleList />", () => {
-  render(
-    <Theme>
-      <VehicleList />
-    </Theme>
-  );
+jest.mock("../../utils/fetchData");
+
+const Container = () => (
+  <Theme>
+    <VehicleList />
+  </Theme>
+);
+
+describe("<VehicleList />", () => {
+  test("Makes a api request successfully", async () => {
+    render(<Container />);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledTimes(1);
+    expect(fetchData).toHaveBeenCalledWith(apiUrl);
+  });
+
+  test("When api request fails displays error message", async () => {
+    (fetchData as jest.Mock).mockRejectedValueOnce("Error!");
+    render(<Container />);
+    expect(fetchData).toHaveBeenCalledTimes(1);
+    expect(fetchData).toHaveBeenCalledWith(apiUrl);
+
+    await waitFor(() => {
+      expect(screen.getByText("Something went wrong!")).toBeInTheDocument();
+    });
+  });
 });
--- client/src/components/__tests__/VehicleProfile.test.tsx
+++ client/src/components/__tests__/VehicleProfile.test.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import VehicleProfile from "../VehicleProfile";
 import Theme from "../../Theme";
+import { fetchData } from "../../utils/fetchData";
+import { apiUrl } from "../VehicleList";
 
-const props = { id: "bmw1", name: "BMW", imgUrl: "http://bmw.com", index: 1 };
+jest.mock("../../utils/fetchData");
+
+const props = { id: "ftype", name: "BMW", imgUrl: "http://bmw.com", index: 1 };
+
+const mockedResponse = {
+  id: "xe",
+  description: "Great vehicle",
+  price: "£30,000",
+};
+
+const Container = () => (
+  <Theme>
+    <VehicleProfile
+      index={props.index}
+      name={props.name}
+      imgUrl={props.imgUrl}
+      id={props.id}
+    />
+  </Theme>
+);
 
 describe("<VehicleProfile />", () => {
   test("displays its props", async () => {
@@ -21,5 +42,30 @@ describe("<VehicleProfile />", () => {
     const imgElem = await screen.findByAltText(props.name, { exact: true });
     expect(vehicleName).toBeInTheDocument();
     expect(imgElem).toBeInTheDocument();
+  });
+
+  test("Fetches vehicle price, description & displays them", async () => {
+    (fetchData as jest.Mock).mockResolvedValueOnce(mockedResponse);
+    render(<Container />);
+    expect(props.id).toBeTruthy();
+    expect(fetchData).toHaveBeenCalledTimes(1);
+    expect(fetchData).toHaveBeenCalledWith(`${apiUrl}/${props.id}`);
+    await waitFor(() => {
+      expect(screen.getByText(mockedResponse.description)).toBeInTheDocument();
+      expect(
+        screen.getByText(`From ${mockedResponse.price}`)
+      ).toBeInTheDocument();
+    });
+  });
+
+  test("When api req fails displays, error message", async () => {
+    (fetchData as jest.Mock).mockRejectedValueOnce("Error!");
+    render(<Container />);
+    expect(props.id).toBeTruthy();
+    expect(fetchData).toHaveBeenCalledTimes(1);
+    expect(fetchData).toHaveBeenCalledWith(`${apiUrl}/${props.id}`);
+    await waitFor(() => {
+      expect(screen.getByText("Something went wrong!")).toBeInTheDocument();
+    });
   });
 });
